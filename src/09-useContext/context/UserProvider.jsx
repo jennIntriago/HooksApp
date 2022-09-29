2ca2c0f05a67734c76ed6988e6fc9bf334e0c6da
Jennifer Intriago <jenniferintriago28@outlook.es>
2022-09-28 22:05:43 -0500

    uso del useContex

--- src/09-useContext/context/UserProvider.jsx
+++ src/09-useContext/context/UserProvider.jsx
@@ -1,13 +1,12 @@
+import { useState } from "react";
 import { UserContext } from "./UserContext";
 
-const user = {
-  id: 123,
-  name: "Jennifer Intriago",
-  email: "jenn@gmail.com",
-};
 export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState();
+
   return (
-    <UserContext.Provider value={{ hola: "Mundo", user: user }}>
+    // <UserContext.Provider value={{ hola: "Mundo", user: user }}>
+    <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
   );
